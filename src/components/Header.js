6659f23bb73f03e24f5fc4176a92f77ff8553558
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-06 15:33:27 +0000

    Add discover btn with viewport drop onClick

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,17 @@ import React from "react";
 
 function Header(props) {
 
+const handleClick = (event) => {
+  event.preventDefault();
+  window.scrollBy(0, window.innerHeight)
+  console.log("click registered")
+}
 
   return (
     <div className="App-header">
         <img className="App-logo" src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt="NASA"/>
         <h1> Photo of the Day </h1>
-        <button className="App-link" > Discover today </button>
+        <a href="#" onClick={handleClick} className="App-link" > Discover today </a>
     </div>
   );
 }
