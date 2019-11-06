6be22ecb133ed927d1f03ddc72740ce2124f5488
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-06 12:46:45 +0000

    Add useEffect to fetch data in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from './components/Header';
 import PictureCard from './components/PictureCard';
 import PreviousDates from './components/PreviousDates';
+import axios from "axios";
 
 
 function App() {
+
+    const [photoState, setPhotoState] = useState([]);
+    const [pastPhotoState, setPastPhotoState] = useState([]);
+
+    useEffect(() => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(`error collecting data`)
+        })
+    }, [])
 
 
   return (
@@ -13,10 +27,6 @@ function App() {
       <Header />
       <PictureCard />
       <PreviousDates /> 
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
     </div>
   );
 }
