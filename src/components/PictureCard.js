5760caf24044f32bcc05a5a301d6ed5a93710c97
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-07 14:10:00 +0000

    Refactored PictureCard.js into style components

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,4 +1,35 @@
 import React  from "react";
+import styled from 'styled-components';
+
+const PictureCardDiv = styled.div`
+  line-height: 1.8;
+  width: 80%;
+  max-width: 900px;
+  margin: 50px auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-size: 3rem;
+  color:  #282c34;
+  text-shadow: 2px 1px #93A2E1;
+`;
+
+const Photo = styled.img`
+  width: 100%;
+`;
+
+const H3 = styled.h3`
+  font-size: 1.6rem;
+  color:  #282c34;
+`;
+
+const Info = styled.p`
+  color:  #282c34;
+  padding: 0px 40px;
+  text-align: left;
+`;
+
 
 
 function PictureCard(props) {
@@ -6,15 +37,13 @@ function PictureCard(props) {
   console.log(props);
   
   return (
-    <div className="pictureCard">
-      <h1> {props.photoData.title} </h1>
-      <span> {props.photoData.date} </span>
-      <img className="mainPhoto" src={props.photoState} alt={props.photoData.title}/>
-      <div>
-        <h3>{props.photoData.title} : Info</h3>
-        <p className="photoInfo">{props.photoData.explanation}</p>
-      </div>
-    </div>
+    <PictureCardDiv>
+      <H1> {props.photoData.title} </H1>
+      <H3> {props.photoData.date} </H3>
+      <Photo src={props.photoState} alt={props.photoData.title}/>
+      <H3>{props.photoData.title}: Info</H3>
+      <Info>{props.photoData.explanation}</Info>
+    </PictureCardDiv>
   );
 }
 
